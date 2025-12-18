3042046ad0962c261e399e631d519e6847f664c6
bala-amypo <bala@amypo.in>
2025-12-18 18:32:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/PurchaseOrder.java
+++ src/main/java/com/example/demo/entity/PurchaseOrder.java
@@ -1,6 +1,95 @@
 package com.example.demo.entity;
 
-@Entity
-public class PurchaseOrder{
+import java.math.BigDecimal;
+import java.sql.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
+public class PurchaseOrder {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true)
+    private String poNumber;
+
+    @Positive
+    private BigDecimal amount;
+
+    @PastOrPresent
+    private Date dateIssued;
+
+    private String approvedBy;
+
+    private String notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPoNumber() {
+        return poNumber;
+    }
+
+    public void setPoNumber(String poNumber) {
+        this.poNumber = poNumber;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Date getDateIssued() {
+        return dateIssued;
+    }
+
+    public void setDateIssued(Date dateIssued) {
+        this.dateIssued = dateIssued;
+    }
+
+    public String getApprovedBy() {
+        return approvedBy;
+    }
+
+    public void setApprovedBy(String approvedBy) {
+        this.approvedBy = approvedBy;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public PurchaseOrder(Long id, String poNumber, @Positive BigDecimal amount, @PastOrPresent Date dateIssued,
+            String approvedBy, String notes) {
+        this.id = id;
+        this.poNumber = poNumber;
+        this.amount = amount;
+        this.dateIssued = dateIssued;
+        this.approvedBy = approvedBy;
+        this.notes = notes;
+    }
+
+    public PurchaseOrder() {
+    }
+
+    
+
+    
 }
--- src/main/java/com/example/demo/entity/SpendCategory.java
+++ src/main/java/com/example/demo/entity/SpendCategory.java
@@ -1,6 +1,63 @@
 package com.example.demo.entity;
 
-@Entity
-public class SpendCategory{
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+public class SpendCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true)
+    private String name;
+
+    private String description;
+
+    boolean active=true;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public SpendCategory(Long id, String name, String description, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.active = active;
+    }
+
+    public SpendCategory() {
+    }
+
+    
 }
