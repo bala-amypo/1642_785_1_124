4e49abfacde554054e348cb6bb11874af1a89579
bala-amypo <bala@amypo.in>
2025-12-20 18:24:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/PurchaseOrderController.java
+++ src/main/java/com/example/demo/controller/PurchaseOrderController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.entity.PurchaseOrder;
 import com.example.demo.service.PurchaseOrderService;
+
 
 @RestController
 public class PurchaseOrderController {
