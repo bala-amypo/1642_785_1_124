475cafd6578cc08cbe3a58774c9bc4be4b035831
bala-amypo <bala@amypo.in>
2025-12-20 18:04:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/PurchaseOrderController.java
+++ src/main/java/com/example/demo/controller/PurchaseOrderController.java
@@ -1,5 +1,120 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// public class PurchaseOrderController{
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.PurchaseOrder;
+import com.example.demo.service.PurchaseOrderService;
+
+@RestController
+public class PurchaseOrderController {
+    private final PurchaseOrderService pos;
+    public PurchaseOrderController(PurchaseOrderService pos){
+        this.pos=pos;
+    }
     
-// }
+    @PostMapping("/POST/purchase-orders")
+    public PurchaseOrder createPurchaseeOrder(PurchaseOrder po){
+        return pos.createPurchaseOrder(po);
+    }
+
+    
+    @PutMapping("/PUT/purchase-orders/{id}")
+    public PurchaseOrder updatePurchaseeOrder(Long id,PurchaseOrder po){
+          return pos.updatePurchaseOrder(id, po);
+    }
+
+    @GetMapping("/GET/purchase-orders/{id}")
+    public PurchaseOrder getPurchaseeOrderByID(Long id){
+         return pos.getPurchaseOrderByID(id);
+    }
+
+    @GetMapping("/GET/purchase-orders")
+    public List<PurchaseOrder>getAllPurchaseeOrders(){
+         return pos.getAllPurchaseOrders();
+    }
+
+    @GetMapping("/GET/supplierId")
+    public List<PurchaseOrder>getPurchaseeOrdersBySupplier(Long supplierId){
+        return pos.getPurchaseOrdersBySupplier(supplierId);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+PurchaseOrderImpl.java:
+package com.example.demo.service.Impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.PurchaseOrder;
+import com.example.demo.repository.PurchaseOrderRepository;
+import com.example.demo.service.PurchaseOrderService;
+
+@Service
+public class PurchaseOrderServiceImpl implements PurchaseOrderService{
+    private final PurchaseOrderRepository por;
+    public PurchaseOrderServiceImpl(PurchaseOrderRepository por){
+        this.por=por;
+    }
+
+    @Override
+    public PurchaseOrder createPurchaseOrder(PurchaseOrder po){
+        return por.save(po);
+    }
+
+    @Override
+    public PurchaseOrder updatePurchaseOrder(Long id,PurchaseOrder po){
+        if(por.existsById(id)){
+            po.setId(id);
+            return por.save(po);
+        }
+        return null;
+    }
+
+    @Override
+    public PurchaseOrder getPurchaseOrderByID(Long id){
+         return por.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<PurchaseOrder>getAllPurchaseOrders(){
+          return por.findAll();
+    }
+
+    @Override
+    public List<PurchaseOrder>getPurchaseOrdersBySupplier(Long supplierId){
+          return por.findBySupplierIdContaining(supplierId);
+    }
+}
+
+
+PurchaseOrdewrService.java
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.PurchaseOrder;
+
+public interface PurchaseOrderService {
+    PurchaseOrder createPurchaseOrder(PurchaseOrder po);
+    PurchaseOrder updatePurchaseOrder(Long id,PurchaseOrder po);
+    PurchaseOrder getPurchaseOrderByID(Long id);
+    List<PurchaseOrder>getAllPurchaseOrders();
+    List<PurchaseOrder>getPurchaseOrdersBySupplier(Long supplierId);
+}
--- src/main/java/com/example/demo/entity/PurchaseOrder.java
+++ src/main/java/com/example/demo/entity/PurchaseOrder.java
@@ -1,15 +1,17 @@
 package com.example.demo.entity;
 
 import java.math.BigDecimal;
-import java.sql.Date;
+import java.time.LocalDate;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
 
+@Entity
 public class PurchaseOrder {
      @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,11 +24,15 @@ public class PurchaseOrder {
     private BigDecimal amount;
 
     @PastOrPresent
-    private Date dateIssued;
+    private LocalDate dateIssued;
 
     private String approvedBy;
 
     private String notes;
+
+    private Supplier supplier;
+
+    private SpendCategory category;
 
     public Long getId() {
         return id;
@@ -52,11 +58,11 @@ public class PurchaseOrder {
         this.amount = amount;
     }
 
-    public Date getDateIssued() {
+    public LocalDate getDateIssued() {
         return dateIssued;
     }
 
-    public void setDateIssued(Date dateIssued) {
+    public void setDateIssued(LocalDate dateIssued) {
         this.dateIssued = dateIssued;
     }
 
@@ -76,19 +82,37 @@ public class PurchaseOrder {
         this.notes = notes;
     }
 
-    public PurchaseOrder(Long id, String poNumber, @Positive BigDecimal amount, @PastOrPresent Date dateIssued,
-            String approvedBy, String notes) {
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public SpendCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(SpendCategory category) {
+        this.category = category;
+    }
+
+    public PurchaseOrder(Long id, String poNumber, @Positive BigDecimal amount, @PastOrPresent LocalDate dateIssued,
+            String approvedBy, String notes, Supplier supplier, SpendCategory category) {
         this.id = id;
         this.poNumber = poNumber;
         this.amount = amount;
         this.dateIssued = dateIssued;
         this.approvedBy = approvedBy;
         this.notes = notes;
+        this.supplier = supplier;
+        this.category = category;
     }
 
     public PurchaseOrder() {
     }
-
+     
     
 
     
