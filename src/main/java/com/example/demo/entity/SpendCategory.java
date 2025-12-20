fde8f6d8f8af82f07014eccc5883d53d4e988dbb
bala-amypo <bala@amypo.in>
2025-12-20 15:26:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/SpendCategory.java
+++ src/main/java/com/example/demo/entity/SpendCategory.java
@@ -1,63 +1,63 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
-// import jakarta.persistence.Column;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-// public class SpendCategory {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
+public class SpendCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//     @Column(unique=true)
-//     private String name;
+    @Column(unique=true)
+    private String name;
 
-//     private String description;
+    private String description;
 
-//     boolean active=true;
+    boolean active=true;
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     public String getName() {
-//         return name;
-//     }
+    public String getName() {
+        return name;
+    }
 
-//     public void setName(String name) {
-//         this.name = name;
-//     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-//     public String getDescription() {
-//         return description;
-//     }
+    public String getDescription() {
+        return description;
+    }
 
-//     public void setDescription(String description) {
-//         this.description = description;
-//     }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-//     public boolean isActive() {
-//         return active;
-//     }
+    public boolean isActive() {
+        return active;
+    }
 
-//     public void setActive(boolean active) {
-//         this.active = active;
-//     }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 
-//     public SpendCategory(Long id, String name, String description, boolean active) {
-//         this.id = id;
-//         this.name = name;
-//         this.description = description;
-//         this.active = active;
-//     }
+    public SpendCategory(Long id, String name, String description, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.active = active;
+    }
 
-//     public SpendCategory() {
-//     }
+    public SpendCategory() {
+    }
 
     
-// }
+}
