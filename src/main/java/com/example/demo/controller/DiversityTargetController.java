c90b0230aa3f8bab80ecd54817795d6c2eb34245
bala-amypo <bala@amypo.in>
2025-12-20 20:31:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/DiversityTargetController.java
+++ src/main/java/com/example/demo/controller/DiversityTargetController.java
@@ -57,18 +57,3 @@ public class DiversityTargetController {
 
 
 
-DiversityTargetService:
-package com.example.demo.service;
-
-import java.util.List;
-
-import com.example.demo.entity.DiversityTarget;
-
-public interface DiversityTargetService {
-    DiversityTarget createTarget(DiversityTarget target);
-    DiversityTarget updateTarget(Long id,DiversityTarget target);
-    List<DiversityTarget> getTargetsByYear(Integer year);
-    List<DiversityTarget> getAllTargets();
-    void deactivateTarget(Long id);
-    
-}
--- src/main/java/com/example/demo/service/DiversityTargetService.java
+++ src/main/java/com/example/demo/service/DiversityTargetService.java
@@ -1,5 +1,14 @@
-// package com.example.demo.service;
+package com.example.demo.service;
 
-// public interface DiversityTargetService{
+import java.util.List;
+
+import com.example.demo.entity.DiversityTarget;
+
+public interface DiversityTargetService {
+    DiversityTarget createTarget(DiversityTarget target);
+    DiversityTarget updateTarget(Long id,DiversityTarget target);
+    List<DiversityTarget> getTargetsByYear(Integer year);
+    List<DiversityTarget> getAllTargets();
+    void deactivateTarget(Long id);
     
-// }
+}
