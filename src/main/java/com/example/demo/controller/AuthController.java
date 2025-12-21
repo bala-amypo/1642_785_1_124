e134fd06775d7f591d9ffa5a35885fdd909a25a3
bala-amypo <bala@amypo.in>
2025-12-21 12:31:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,82 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// public class AuthController{
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-// }
+import com.example.demo.entity.UserAccount;
+import com.example.demo.service.UserAccountService;
+
+@RestController
+public class AuthController {
+   private final UserAccountService uas;
+   public AuthController(UserAccountService uas){
+         this.uas=uas;
+   }
+    
+   @PostMapping("/POST/user-account")
+    public String registerUser(@RequestBody UserAccount user){
+        return uas.register(user);
+    }
+
+    @GetMapping("/GET/find-user")
+    public UserAccount findUserByEmailOrThrow(@RequestParam String email){
+        return uas.findByEmailOrThrow(email);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+UserAccountService.java
+package com.example.demo.service;
+
+import com.example.demo.entity.UserAccount;
+
+public interface UserAccountService {
+    String register(UserAccount user);
+    UserAccount findByEmailOrThrow(String email);
+}
+
+UserAccountServiceImpl.java
+package com.example.demo.service.Impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.UserAccount;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.service.UserAccountService;
+
+@Service
+public class UserAccountServiceImpl implements UserAccountService{
+    private final UserAccountRepository uar;
+    public UserAccountServiceImpl(UserAccountRepository uar){
+        this.uar=uar;
+    }
+    @Override
+    public String register(UserAccount user){
+        uar.save(user);
+        return "User registered successfully";
+        
+    }
+
+    @Override
+    public UserAccount findByEmailOrThrow(String email){
+         UserAccount user=uar.findByEmail(email);
+         if(user==null){
+            throw new ResourceNotFoundException("Given email is not registered");
+         }
+         return user;
+    }
+
+}
--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -9,3 +9,14 @@
 // public interface UserAccountRepository extends JpaRepository<UserAccount,Long>{
     
 // }
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.UserAccount;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount,Long>{
+    UserAccount findByEmail(String email);
+}
