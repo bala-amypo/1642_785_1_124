2b47c848d1c37e7d5e27d3ce8670c78e2433414f
bala-amypo <bala@amypo.in>
2025-12-23 16:20:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/DiversityClassification.java
+++ src/main/java/com/example/demo/entity/DiversityClassification.java
@@ -1,3 +1,63 @@
+// package com.example.demo.entity;
+
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.validation.constraints.NotNull;
+
+// @Entity
+// public class DiversityClassification {
+//      @Id
+//      @GeneratedValue(strategy = GenerationType.IDENTITY)
+//      private Long id;
+
+//      @NotNull
+//      @Column(unique=true)
+//      private String code;
+
+//      @NotNull
+//      private String description;
+
+//      Boolean active=true;
+     
+//      public Long getId() {
+//          return id;
+//      }
+//      public void setId(Long id) {
+//          this.id = id;
+//      }
+//      public String getCode() {
+//          return code;
+//      }
+//      public void setCode(String code) {
+//          this.code = code;
+//      }
+//      public String getDescription() {
+//          return description;
+//      }
+//      public void setDescription(String description) {
+//          this.description = description;
+//      }
+//      public Boolean getActive() {
+//          return active;
+//      }
+//      public void setActive(Boolean active) {
+//          this.active = active;
+//      }
+//      public DiversityClassification(Long id, String code, String description, Boolean active) {
+//         this.id = id;
+//         this.code = code;
+//         this.description = description;
+//         this.active = active;
+//      }
+//      public DiversityClassification() {
+//      }
+     
+     
+// }
+
 package com.example.demo.entity;
 
 import jakarta.persistence.Column;
@@ -57,6 +117,18 @@ public class DiversityClassification {
      
      
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/com/example/demo/entity/Supplier.java
+++ src/main/java/com/example/demo/entity/Supplier.java
@@ -1,3 +1,150 @@
+// package com.example.demo.entity;
+
+// import java.time.LocalDateTime;
+
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.PrePersist;
+// import jakarta.persistence.PreUpdate;
+// import jakarta.validation.constraints.Email;
+// import jakarta.validation.constraints.NotNull;
+
+// @Entity
+// public class Supplier {
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.IDENTITY)
+//     private Long id;
+
+//     @Column(unique=true,nullable=true)
+//     private String name;
+
+//     @Column(unique=true,nullable=true)
+//     private String registrationNumber;
+
+//     @Column(unique=true)
+//     @Email
+//     private String email;
+
+//     @NotNull
+//     private String phone;
+
+//     @NotNull
+//     private String address;
+
+//     Boolean isActive=true;
+
+//     private LocalDateTime createdAt;
+//     private LocalDateTime updatedAt;
+
+//     @PrePersist
+//     public void onCreate(){
+//         LocalDateTime now=LocalDateTime.now();
+//         if(createdAt==null){
+//             this.createdAt=now;
+//         }
+//         this.updatedAt=now;
+//     }
+
+//     @PreUpdate
+//     public void onUpdate(){
+//         LocalDateTime now=LocalDateTime.now();
+//         this.updatedAt=now;
+//     }
+
+//     public Long getId() {
+//         return id;
+//     }
+
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
+
+//     public String getName() {
+//         return name;
+//     }
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     public String getRegistrationNumber() {
+//         return registrationNumber;
+//     }
+
+//     public void setRegistrationNumber(String registrationNumber) {
+//         this.registrationNumber = registrationNumber;
+//     }
+
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
+
+//     public String getPhone() {
+//         return phone;
+//     }
+
+//     public void setPhone(String phone) {
+//         this.phone = phone;
+//     }
+
+//     public String getAddress() {
+//         return address;
+//     }
+
+//     public void setAddress(String address) {
+//         this.address = address;
+//     }
+
+//     public Boolean getIsActive() {
+//         return isActive;
+//     }
+
+//     public void setIsActive(Boolean isActive) {
+//         this.isActive = isActive;
+//     }
+
+//     public LocalDateTime getCreatedAt() {
+//         return createdAt;
+//     }
+
+//     public void setCreatedAt(LocalDateTime createdAt) {
+//         this.createdAt = createdAt;
+//     }
+
+//     public LocalDateTime getUpdatedAt() {
+//         return updatedAt;
+//     }
+
+//     public void setUpdatedAt(LocalDateTime updatedAt) {
+//         this.updatedAt = updatedAt;
+//     }
+
+//     public Supplier(Long id, String name, @NotNull String registrationNumber, @Email String email, String phone,
+//             String address, Boolean isActive, LocalDateTime createdAt, LocalDateTime updatedAt) {
+//         this.id = id;
+//         this.name = name;
+//         this.registrationNumber = registrationNumber;
+//         this.email = email;
+//         this.phone = phone;
+//         this.address = address;
+//         this.isActive = isActive;
+//         this.createdAt = createdAt;
+//         this.updatedAt = updatedAt;
+//     }
+
+//     public Supplier() {
+//     }
+    
+    
+// }
+
 package com.example.demo.entity;
 
 import java.time.LocalDateTime;
