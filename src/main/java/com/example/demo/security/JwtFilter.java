91643a7c06b5867e35222632c1c45069f9a8ca62
bala-amypo <bala@amypo.in>
2025-12-26 18:29:57 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtFilter.java
+++ src/main/java/com/example/demo/security/JwtFilter.java
@@ -14,6 +14,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+package com.example.demo.security.CustomUserDetailsService;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter{
