eead55f9bba21afb77b56dc57be67c4149f2b937
bala-amypo <bala@amypo.in>
2025-12-27 09:06:47 +0000

    User commit

--- src/main/java/com/example/demo/service/PurchaseOrderService.java
+++ src/main/java/com/example/demo/service/PurchaseOrderService.java
@@ -20,7 +20,7 @@ import java.util.List;
 public interface PurchaseOrderService {
     PurchaseOrder createPurchaseOrder(PurchaseOrder purchaseOrder);
     List<PurchaseOrder> getPurchaseOrdersBySupplier(Long supplierId);
-    PurchaseOrder createPurchaseOrder(PurchaseOrder po);
     PurchaseOrder updatePurchaseOrder(Long id,PurchaseOrder po);
     PurchaseOrder getPurchaseOrderByID(Long id);
+    List<PurchaseOrder>getAllPurchaseOrders();
 }
