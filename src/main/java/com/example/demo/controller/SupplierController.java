54f4f8582a9e8fa2dd01d4c399c91f4e021cfbab
bala-amypo <bala@amypo.in>
2025-12-27 10:06:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/SupplierController.java
+++ src/main/java/com/example/demo/controller/SupplierController.java
@@ -19,30 +19,30 @@
 //         this.ss=ss;
 //     }
 
-//     @PostMapping("/POST/suppliers")
-//     public Supplier postSupplier(@RequestBody Supplier supplier){
-//           return ss.createSupplier(supplier);
-//     }
+    // @PostMapping("/POST/suppliers")
+    // public Supplier postSupplier(@RequestBody Supplier supplier){
+    //       return ss.createSupplier(supplier);
+    // }
 
-//     @PutMapping("/PUT/suppliers/{id}")
-//     public Supplier updateValue(@PathVariable Long id,@RequestBody Supplier supplier){
-//         return ss.updateSupplier(id,supplier);
-//     }
+    // @PutMapping("/PUT/suppliers/{id}")
+    // public Supplier updateValue(@PathVariable Long id,@RequestBody Supplier supplier){
+    //     return ss.updateSupplier(id,supplier);
+    // }
 
-//     @GetMapping("/GET/suppliers/{id}")
-//     public Supplier retrieveSupplierById(@PathVariable Long id){
-//        return ss.getSupplierById(id);
-//     }
+    // @GetMapping("/GET/suppliers/{id}")
+    // public Supplier retrieveSupplierById(@PathVariable Long id){
+    //    return ss.getSupplierById(id);
+    // }
 
-//     @GetMapping("/GET/suppliers")
-//     public List<Supplier> retrieveSuppliers(){
-//         return ss.getAllSuppliers();
-//     }
+    // @GetMapping("/GET/suppliers")
+    // public List<Supplier> retrieveSuppliers(){
+    //     return ss.getAllSuppliers();
+    // }
 
-//     @PutMapping("/PUT/suppliers/{id}/deactivate")
-//     public void deactivateById(@PathVariable Long id){
-//         ss.deactivateSupplier(id);
-//     }
+    // @PutMapping("/PUT/suppliers/{id}/deactivate")
+    // public void deactivateById(@PathVariable Long id){
+    //     ss.deactivateSupplier(id);
+    // }
 // }
 
 package com.example.demo.controller;
@@ -50,6 +50,18 @@ package com.example.demo.controller;
 import com.example.demo.service.SupplierService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Supplier;
+import com.example.demo.service.SupplierService;
 @RestController
 @RequestMapping("/supplier")
 public class SupplierController {
@@ -57,5 +69,30 @@ public class SupplierController {
     
     public SupplierController(SupplierService supplierService) {
         this.supplierService = supplierService;
+    }
+
+    @PostMapping("/POST/suppliers")
+    public Supplier postSupplier(@RequestBody Supplier supplier){
+          return ss.createSupplier(supplier);
+    }
+
+    @PutMapping("/PUT/suppliers/{id}")
+    public Supplier updateValue(@PathVariable Long id,@RequestBody Supplier supplier){
+        return ss.updateSupplier(id,supplier);
+    }
+
+    @GetMapping("/GET/suppliers/{id}")
+    public Supplier retrieveSupplierById(@PathVariable Long id){
+       return ss.getSupplierById(id);
+    }
+
+    @GetMapping("/GET/suppliers")
+    public List<Supplier> retrieveSuppliers(){
+        return ss.getAllSuppliers();
+    }
+
+    @PutMapping("/PUT/suppliers/{id}/deactivate")
+    public void deactivateById(@PathVariable Long id){
+        ss.deactivateSupplier(id);
     }
 }
