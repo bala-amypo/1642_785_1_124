01e2af9f2be66492551ea55131b4859b684f75d8
bala-amypo <bala@amypo.in>
2025-12-27 08:57:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/PurchaseOrderController.java
+++ src/main/java/com/example/demo/controller/PurchaseOrderController.java
@@ -49,9 +49,17 @@
 
 package com.example.demo.controller;
 
-import com.example.demo.service.PurchaseOrderService;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.entity.PurchaseOrder;
+import com.example.demo.service.PurchaseOrderService;
+
 @RestController
 @RequestMapping("/purchaseorders")
 public class PurchaseOrderController {
@@ -60,6 +68,33 @@ public class PurchaseOrderController {
     public PurchaseOrderController(PurchaseOrderService service) {
         this.service = service;
     }
+
+    @PostMapping("/POST/purchase-orders")
+    public PurchaseOrder createPurchaseeOrder(@RequestBody PurchaseOrder po){
+        return service.createPurchaseOrder(po);
+    }
+
+    
+    @PutMapping("/PUT/purchase-orders/{id}")
+    public PurchaseOrder updatePurchaseeOrder(@PathVariable Long id,@RequestBody PurchaseOrder po){
+          return service.updatePurchaseOrder(id, po);
+    }
+
+    @GetMapping("/GET/purchase-orders/{id}")
+    public PurchaseOrder getPurchaseeOrderByID(@PathVariable Long id){
+         return service.getPurchaseOrderByID(id);
+    }
+
+    @GetMapping("/GET/purchase-orderss")
+    public List<PurchaseOrder>getAllPurchaseeOrders(){
+         return service.getAllPurchaseOrders();
+    }
+
+    @GetMapping("/GET/{supplierId}")
+    public List<PurchaseOrder>getPurchaseeOrdersBySupplier(Long supplierId){
+        return service.getPurchaseOrdersBySupplier(supplierId);
+    }
+}
 }
 
 
