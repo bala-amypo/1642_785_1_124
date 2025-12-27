246df7dc97338e3cfcdb4f9b3f1acf19838c72ff
bala-amypo <bala@amypo.in>
2025-12-27 09:39:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/SpendCategoryController.java
+++ src/main/java/com/example/demo/controller/SpendCategoryController.java
@@ -45,9 +45,18 @@
 // }
 
 package com.example.demo.controller;
-
-import com.example.demo.service.SpendCategoryService;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.SpendCategory;
+import com.example.demo.service.SpendCategoryService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/spendcategory")
