871f23aec165f01bf6e0754de1801a0366957127
bala-amypo <bala@amypo.in>
2025-12-27 05:33:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/DiversityClassificationController.java
+++ src/main/java/com/example/demo/controller/DiversityClassificationController.java
@@ -47,62 +47,18 @@
 //     }
 // }
 
-// package com.example.demo.controller;
-
-// import com.example.demo.service.DiversityClassificationService;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// @RestController
-// @RequestMapping("/diversityclassification")
-// public class DiversityClassificationController {
-//     private final DiversityClassificationService service;
-    
-//     public DiversityClassificationController(DiversityClassificationService service) {
-//         this.service = service;
-//     }
-// }
-
 package com.example.demo.controller;
 
-import com.example.demo.entity.DiversityClassification;
 import com.example.demo.service.DiversityClassificationService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/diversityclassification")
 public class DiversityClassificationController {
-
     private final DiversityClassificationService service;
-
+    
     public DiversityClassificationController(DiversityClassificationService service) {
         this.service = service;
     }
-
-    @PostMapping
-    public DiversityClassification create(@RequestBody DiversityClassification obj) {
-        return service.save(obj);
-    }
-
-    @GetMapping
-    public List<DiversityClassification> getAll() {
-        return service.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public DiversityClassification getById(@PathVariable Long id) {
-        return service.findById(id);
-    }
-
-    @PutMapping("/{id}")
-    public DiversityClassification update(@PathVariable Long id,
-                                          @RequestBody DiversityClassification obj) {
-        return service.update(id, obj);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        service.delete(id);
-    }
 }
+
